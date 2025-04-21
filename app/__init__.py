c6b937068eb11044d55fd183f9f2a5b073092859
Kris Dorohina <sun.08.90@mail.ru>
2025-04-21 14:09:49 +0300

    Initial commit: базовый функционал ресторана на Flask

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,13 @@ from flask_login import LoginManager
 from config import Config
 
 db = SQLAlchemy()
+app = Flask(__name__)
 login_manager = LoginManager()
 
 
 def create_app():
-    app = Flask(__name__)
     app.config.from_object(Config)
-
     db.init_app(app)
+
+    from app import routes
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,7 @@
 """Здесь все маршруты"""
+from flask import render_template
+from app import app
+
+@app.route('/')
+def home():
+    return render_template('index.html')
